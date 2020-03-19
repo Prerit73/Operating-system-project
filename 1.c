70add284a4c439c1b975df028bcdc779bf36a918
Prerit73 <57189948+Prerit73@users.noreply.github.com>
2020-03-19 21:40:45 +0530

    Update 1.c

--- 1.c
+++ 1.c
@@ -9,3 +9,34 @@ int arrival_t,burst_t,ct,wait_t,taround_time,p;
 
 int flag;
  }
+p_list[100];
+ void Sorting()
+ {
+struct process p;
+
+int i, j;
+
+for(i=0;i<n-1;i++)
+
+{
+
+for(j=i+1;j<n;j++)
+
+{
+
+if(p_list[i].arrival_t > p_list[j].arrival_t)
+
+{
+
+p = p_list[i];
+
+p_list[i] = p_list[j];
+
+p_list[j] = p;
+
+}
+
+}
+
+}
+ }
