0e0b83c48a5c07615a78dcc761c20c075a23973d
Prerit73 <57189948+Prerit73@users.noreply.github.com>
2020-03-28 11:47:29 +0530

    Update 1.c

--- 1.c
+++ 1.c
@@ -40,3 +40,30 @@ p_list[j] = p;
 
 }
  }
+int main()
+ {
+
+int i,t=0,b_t=0,peak;
+
+int a[10];
+
+float wait_time = 0, taround_time = 0, avg_w_t=0, avg_taround_time=0;
+
+printf("enter the no. of processes: ");
+
+scanf("%d",&n);
+
+for(i = 0; i < n; i++)
+
+{
+
+p_list[i].p_no = i+1;
+
+printf("\nEnter Details For P%d process:-\n", p_list[i].p_no);
+printf("Enter Arrival Time: ");
+scanf("%d", &p_list[i].arrival_t );
+printf("Enter Burst Time: ");
+scanf("%d", &p_list[i].burst_t);
+p_list[i].flag = 0;
+b_t = b_t + p_list[i].burst_t;
+}
