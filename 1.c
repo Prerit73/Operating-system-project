d8138f13716eeced38fbcbdb9a8053c09dc34b76
Prerit73 <57189948+Prerit73@users.noreply.github.com>
2020-04-18 15:01:31 +0530

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,83 +1,41 @@
 #include<stdio.h>
-int n;
-struct process
-{
-int p_no;
-int arrival_t,burst_t,ct,wait_t,taround_time,p;
-int flag;
- }
-p_list[100];
- void Sorting()
- {
-struct process p;
-int i, j;
-for(i=0;i<n-1;i++)
-{
-for(j=i+1;j<n;j++)
-{
-if(p_list[i].arrival_t > p_list[j].arrival_t)
-{
-p = p_list[i];
-p_list[i] = p_list[j];
-p_list[j] = p;
-}}}}
 int main()
- { 
- int i,t=0,b_t=0,peak;
-int a[10];
-float wait_time = 0, taround_time = 0, avg_w_t=0, avg_taround_time=0;
-printf("enter the no. of processes: ");
-scanf("%d",&n);
-for(i = 0; i < n; i++)
 {
-p_list[i].p_no = i+1;
-
-printf("\nEnter Details For P%d process:-\n", p_list[i].p_no);
-printf("Enter Arrival Time: ");
-scanf("%d", &p_list[i].arrival_t );
-printf("Enter Burst Time: ");
-scanf("%d", &p_list[i].burst_t);
-p_list[i].flag = 0;
-b_t = b_t + p_list[i].burst_t;
-}
-Sorting();
-for(int i=0;i<n;i++)
-{
-a[i]=p_list[i].burst_t;
-}
-p_list[9].burst_t = 9999;
-for(t = p_list[0].arrival_t; t <= b_t+1;)
-{
-peak = 9;
-for(i=0;i<n;i++)
-{
-if(p_list[i].arrival_t <= t && p_list[i].burst_t < p_list[peak].burst_t && p_list[i].flag != 1)
-{
-peak = i;
-}
-if(p_list[peak].burst_t==0 && p_list[i].flag != 1)
-{
-p_list[i].flag = 1;
-p_list[peak].ct=t;p_list[peak].burst_t=9999;
-printf("P%d completes in %d\n",p_list[i].p_no,p_list[peak].ct);
-}
-}
-t++;
-(p_list[peak].burst_t)--;
-}
-for(i=0;i<n;i++)
-{
-p_list[i].taround_time=(p_list[i].ct)-(p_list[i].arrival_t);
-avg_taround_time=avg_taround_time+p_list[i].taround_time;
-p_list[i].wait_t=((p_list[i].taround_time)-a[i]);
-avg_w_t=avg_w_t+p_list[i].wait_t;
-}
-printf("PNO\tAT\tCT\tTA\tWTt\n");
-for(i=0;i<n;i++)
-{
-printf("P%d\t%d\t%d\t%d\t%d\n",p_list[i].p_no,p_list[i].arrival_t,p_list[i].ct,p_list[i].taround_time
-,p_list[i].wait_t);
- }
-printf("Average Turn around Time: %f\t\n\n",avg_taround_time);
-printf("Average Waiting Time :\t %f\t\n",avg_w_t);
+    int at[10],bt[10],rt[10],endTime,i,smallest;
+    int remain=0,n,time,sum_wait=0,sum_turnaround=0;
+    printf("Enter no of Processes : ");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        printf("\nEnter arrival time for Process P%d : ",i+1);
+        scanf("%d",&at[i]);
+        printf("Enter burst time for Process P%d : ",i+1);
+        scanf("%d",&bt[i]);
+        rt[i]=bt[i];
+    }
+    printf("\n\nProcess\t|Turnaround Time| Waiting Time\n\n");
+    rt[9]=9999;
+    for(time=0;remain!=n;time++)
+    {
+        smallest=9;
+        for(i=0;i<n;i++)
+        {
+            if(at[i]<=time && rt[i]<rt[smallest] && rt[i]>0)
+            {
+                smallest=i;
+            }
+        }
+        rt[smallest]--;
+        if(rt[smallest]==0)
+        {
+            remain++;
+            endTime=time+1;
+            printf("\nP%d\t|\t%d\t|\t%d",smallest+1,endTime-at[smallest],endTime-bt[smallest]-at[smallest]);
+            sum_wait+=endTime-bt[smallest]-at[smallest];
+            sum_turnaround+=endTime-at[smallest];
+        }
+    }
+    printf("\n\nAverage waiting time = %f\n",sum_wait*1.0/n);
+    printf("Average Turnaround time = %f",sum_turnaround*1.0/5);
+    return 0;
 }
